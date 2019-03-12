bcfa1b829e1008fc08488f750d712c550b1d196a
Derek Brown <d.brown4309@gmail.com>
2019-03-11 23:47:34 -0500

    Added bat, early bat swing script and  grass plane

---
+++ Baseball_game/Assets/Scripts/BatSwing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatSwing : MonoBehaviour
+{
+
+	public float batSpeed;
+    public Vector3 rotation;
+
+    private Transform bat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("space"))
+		{
+            //Mathf.MoveTowards(bat.transform.rotation.y, yRot, Time.time);
+            this.transform.Rotate(rotation, Space.World);
+            
+		}
+
+
+    }
+}
